7dc20a1dee0903c87b70a2d767146ba227bfeb04
Joel Tondozi <joeltondozi@gmail.com>
2023-11-29 18:13:22 +0100

    feat: add create frame fonction

---
+++ controllers/frame.js
@@ -0,0 +1,5 @@
+const { User } = require('../models/user')
+const { Frame } = require('..//models/frame')
+const insertFrame = require('../utils/frame')
+
+module.exports = createFrame = async (req, res) => {}
--- utils/frame.js
+++ utils/frame.js
@@ -5,8 +5,37 @@ module.exports = insertFrame = async (frame, Frame, User) => {
     })
     if (frameExist) {
       throw new Error(
-        `Il est existe déjà un frame qui porte ce nom : ${frameExist.name}`
+        `Il  existe déjà un frame qui porte ce nom : ${frameExist.name}`
       )
     }
-  } catch (error) {}
+    const userCheck = await User.findOne({
+      facebookId: frame.createBy,
+    })
+    if (!userCheck) {
+      throw new Error('Utilisateur introuvable')
+    } else if (userCheck.abonnement >= 0) {
+      throw new Error(' avez plus d abonnement pour creer ce frame')
+    }
+
+    let newFrame = null
+    newFrame = await Frame.create(frame)
+    const userUpdate = await User.update(
+      { facebookId: frame.createBy },
+      { $push: { frames: newFrame.name } }
+    )
+    if (!userUpdate) {
+      throw new Error(
+        'Quelque chose s est mal passée lors de l attribution de la propriéte de ce frame '
+      )
+    }
+    return {
+      frame: newFrame,
+      error: null,
+    }
+  } catch (error) {
+    return {
+      frame: null,
+      error: error.massage,
+    }
+  }
 }
