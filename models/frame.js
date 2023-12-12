7b7b25f4dd6cbe28189f98d52d5f42da2a1f9700
Joel Tondozi <joeltondozi@gmail.com>
2023-12-12 16:41:59 +0100

    feat : add deleted champs on frame mode

--- models/frame.js
+++ models/frame.js
@@ -2,34 +2,41 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const uniqueValidator = require('mongoose-unique-validator')
 
-const frameSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const frameSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    usedBy: {
+      type: [String],
+      default: [],
+    },
+    planId: {
+      type: String,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-  },
-  imgUrl: {
-    type: String,
-    required: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  likes: {
-    type: [String],
-    default: [],
-  },
-  usedBy: {
-    type: [String],
-    default: []
-  },
-  planId: {
-    type: String,
-  }
-}, { timestamps: true })
+  { timestamps: true }
+)
 
 frameSchema.plugin(uniqueValidator)
 const Frame = mongoose.model('frame', frameSchema)
